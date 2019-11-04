e850162d90653133d22a2a236ac100b6e66e02c5
BuketAkgun <56505535+BuketAkgun@users.noreply.github.com>
2019-11-04 09:52:54 +0300

    Bug fixes

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
-from flask import Flask,render_template, flash, redirect, url_for, request
-from forms import RegistrationForm, LoginForm, LoginFormP, RegistrationFormD,Operation
+from flask import Flask,render_template, flash, redirect, url_for, request, session, jsonify
+from forms import RegistrationForm, LoginForm, LoginFormP, RegistrationFormD,Operation,Appointment
 import psycopg2
 
 app = Flask(__name__)
-
 
 DB_NAME = "afoeiapd"
 DB_USER = "afoeiapd"
@@ -22,7 +21,6 @@ def CDB():
         print("FFF")
 
 
-
 @app.route("/")
 @app.route("/home")
 def home():
@@ -36,12 +34,12 @@ def register():
             try:
                 conn = CDB()
                 cur = conn.cursor()
-                statement = "INSERT INTO person (tc,first_name,last_name,email,phone,pass) VALUES ('%d','%s','%s','%s','%d','%s');"       
-                cur.execute(statement,int(request.form['tc']),request.form['first_name'],request.form['last_name'],request.form['email'],int(request.form['phone']),request.form['password'])
+                cur.execute("INSERT INTO person (tc,first_name,last_name,email,phone,pass) VALUES (%s,%s,%s,%s,%s,%s)",
+                (request.form['tc'],request.form['first_name'],request.form['last_name'],request.form['email'],request.form['phone'],request.form['password'],))
                 conn.commit()
                 cur.close()
                 conn.close()
-                return redirect(url_for('home_p'))
+                return redirect(url_for('login'))
             except:
                 return redirect(url_for('register'))
     return render_template("register.html", form = form)
@@ -54,12 +52,16 @@ def register_dr():
             try: 
                 conn = CDB()
                 cur = conn.cursor()
-                statement = "INSERT INTO doctor (tc,first_name,last_name,email,phone,room,dep,pass) VALUES ('%d','%s','%s','%s','%d','%d','%s','%s');"        
-                cur.execute(statement,int(request.form['tc']),request.form['first_name'],request.form['last_name'],request.form['email'],int(request.form['phone']),int(request.form['room']),request.form['department'],request.form['password'])
+                statement = "INSERT INTO doctor (tc,first_name,last_name,email,phone,room,dep,pass) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"        
+                cur.execute(statement,(request.form['tc'],request.form['first_name'],request.form['last_name'],request.form['email'],request.form['phone'],request.form['room'],request.form['department'],request.form['password'],))
+                
                 conn.commit()
+                cur.execute("select extract(dow from now())")
+                day = cur.fetchone()
+                
                 cur.close()
                 conn.close()
-                return redirect(url_for('home_dr'))
+                return redirect(url_for('login_dr'))
             except:
                 return redirect(url_for('home'))
     return render_template("register_dr.html", form = form)
@@ -68,18 +70,16 @@ def register_dr():
 def register_nr():
     form = RegistrationForm()
     if (request.method =='POST'):
-        print(form.errors)
         if form.validate_on_submit():
-            print(form.errors)
             try:
                 conn = CDB()
                 cur = conn.cursor()
-                statement = "INSERT INTO nurse (tc,first_name,last_name,email,phone,pass) VALUES ('%d','%s','%s','%s','%d','%s');"       
-                cur.execute(statement,int(request.form['tc']),request.form['first_name'],request.form['last_name'],request.form['email'],int(request.form['phone']),request.form['password'])
+                statement = "INSERT INTO nurse (tc,first_name,last_name,email,phone,pass) VALUES (%s,%s,%s,%s,%s,%s)"       
+                cur.execute(statement,(request.form['tc'],request.form['first_name'],request.form['last_name'],request.form['email'],request.form['phone'],request.form['password'],))
                 conn.commit()
                 cur.close()
                 conn.close()
-                return redirect(url_for('home_d'))
+                return redirect(url_for('login_nr'))
             except:
                 return redirect(url_for('home'))
     return render_template("register_nr.html", form = form)
@@ -92,8 +92,8 @@ def operation():
             try:
                 conn = CDB()
                 cur = conn.cursor()
-                statement = "INSERT INTO surgery (patient_id,doctor_id,nurse_id,op_room,date,time) VALUES ('%d','%d','%d','%d','%s','%s');"
-                cur.execute(statement,request.form['patient_id'],request.form['doctor_id'],request.form['nurse_id'],request.form['op_room'],request.form['date'],request.form['time'])
+                statement = "INSERT INTO surgery (patient_id,doctor_id,nurse_id,op_room,date,time) VALUES (%s,%s,%s,%s,%s,%s)"
+                cur.execute(statement,(request.form['patient_id'],request.form['doctor_id'],request.form['nurse_id'],request.form['room'],request.form['date'],request.form['time'],))
                 conn.commit()
                 cur.close()
                 conn.close()
@@ -117,6 +117,21 @@ def op_view():
         print("error")
     return render_template("op_view.html", table = result)
 
+@app.route("/prescriptions" , methods=['GET'])
+def pres():
+    try:
+        conn = CDB()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM pres WHERE (person = %s)",(session['tc'],))
+        p = cur.fetchall() 
+        cur.close()
+        conn.close()
+    except:
+        print("error")
+    return render_template("pres.html", table = p)
+
+
+
 @app.route("/home_dr", methods=['GET', 'POST'])
 def home_dr():
     return render_template("home_dr.html")
@@ -125,26 +140,53 @@ def home_dr():
 def home_p():
     return render_template("home_p.html")
 
+@app.route("/sign_out", methods=['GET', 'POST'])
+def sign_out():
+    session.clear()
+    return redirect(url_for('home'))
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginFormP()
-    if form.validate_on_submit():
-        if form.tc.data == '12345678900' and form.password.data == '1234567890':
-            flash('you have been logged in', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('no login', 'danger')
+    if (request.method =='POST'):
+        if form.validate_on_submit():
+            tc = request.form['tc']
+            passw = request.form['password']
+            try:
+                conn = CDB()
+                cur = conn.cursor()
+                cur.execute("SELECT pass FROM person WHERE tc=%s",(tc,))
+                result = cur.fetchone()
+                if(result[0] == passw):
+                    session['tc'] = tc
+                    return redirect(url_for('home_p'))
+                else:
+                    flash('no login', 'danger')
+                    return redirect(url_for('login'))
+            except:
+                print("error")
     return render_template("login.html", form = form)
 
 @app.route("/login_dr", methods=['GET', 'POST'])
 def login_dr():
     form = LoginForm()
-    if form.validate_on_submit():
-        if form.tc.data == '12345678900' and form.password.data == '1234567890':
-            flash('you have been logged in', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('no login', 'danger')
+    if (request.method =='POST'):
+        if form.validate_on_submit():
+            tc = request.form['tc']
+            passw = request.form['password']
+            try:
+                conn = CDB()
+                cur = conn.cursor()
+                cur.execute("SELECT pass FROM doctor WHERE tc=%s",(tc,))
+                result = cur.fetchone()
+                if(result[0] == passw):
+                    return redirect(url_for('home_dr'))
+                else:
+                    flash('no login', 'danger')
+                    return redirect(url_for('login_dr'))
+                cur.close()
+                conn.close()
+            except:
+                print("error")
     return render_template("login_doctor.html", form = form)
 
 @app.route("/login_nr", methods=['GET', 'POST'])
@@ -158,6 +200,30 @@ def login_nr():
             flash('no login', 'danger')
     return render_template("login_nurse.html", form = form)
 
-if __name__ == '__main__':
-    app.run(debug = False)
+@app.route("/make_appointment", methods=['GET', 'POST'])
+def make_appointment():
+    form = Appointment()
+    try:
+        conn = CDB()
+        cur = conn.cursor()
+        cur.execute("SELECT dep FROM doctor ORDER BY dep ASC")
+        dep = cur.fetchall()
+        deps = []
+        for i in range(0,len(dep)):
+            deps.append(tuple((i,dep[i][0])))
+        
+        form.dep.choices = deps
+        if (request.method =='POST'):
+            ph = form.dep.data
+            url = "/make_appointment/{}".format(dep[int(ph)][0])
+            return redirect(url)
+    except:
+        print("error")
+    return render_template("appo.html", form = form)
 
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
